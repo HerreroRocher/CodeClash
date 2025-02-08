5b2ec2655caba9934cbe6d4f5982d0f374e7c31e
Daniel Herrero <danielherrero4465@gmail.com>
2025-02-08 00:19:53 +0000

    feat: add Logo component and enhance How to Play page with detailed instructions

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Logo() {
+  return (
+    <Link href="/" className="inline-block m-5">
+      <Image src="/logo_square.jpg" alt="CodeClash Logo" width={75} height={50} />
+    </Link>
+  );
+}
